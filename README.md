b5b32e6126da4ed625ec6fd576c1c07519bc5532
Julien Déoux <julien.deoux@ac-nancy-metz.fr>
2019-07-19 10:47:00 +0200

    Added Angular mode

--- README.md
+++ README.md
@@ -3,8 +3,22 @@
 Small Python tool which converts an SVG file into its most barebones version, for use within HTML code:
 * Removes useless information from SVG editors such as Inkscape or Illustrator.
 * Removes invisible layers and paths.
+* In Angular mode, removes flow* tags.
+
+## Usage
+
+```shell
+python3 ./svg_cleaner.py (-a | --angular) [SVG_FILE]
+```
+
+### Options
+
+#### `-h, --help`
+Prints the help message
+
+#### `-a, --angular`
+Enables Angular mode
 
 ## TODO
 
-* Create an Angular mode which removes flowRoot elements.
 * Print to standard output to be more POSIX-like.
--- svg_cleaner.py
+++ svg_cleaner.py
@@ -1,13 +1,35 @@
 #!/bin/python3
 import sys
 import os
+import getopt
 import xml.etree.ElementTree as ET
 
-if 2 != len(sys.argv):
-    print('Usage: python3 ./svg_cleaner.py [svg_file]')
-    sys.exit(1)
+HELP = '''Usage: python3 ./svg_cleaner.py (-a | --angular) [SVG_FILE]
+Options :
+    -h, --help                  Prints this help message
+    -a, --angular               Enables Angular mode (removes flow* tags)'''
 
-filename = sys.argv[1]
+# Arguments par défaut
+angular_mode = False
+
+# Récupération des options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'ah', ['--angular', '--help'])
+except getopt.GetoptError:
+    print(HELP)
+    sys.exit(1)
+for opt, arg in opts:
+    if opt in ('-a', '--angular'):
+        angular_mode = True
+    if opt in ('-h', '--help'):
+        print(HELP)
+        exit(0)
+if len(args) >= 1:
+    filename = os.path.abspath(args[0])
+    args = args[1:]
+else:
+    print(HELP)
+    sys.exit(1)
 
 if not os.path.isfile(filename):
     print('file %s not found', filename)
@@ -38,6 +60,15 @@ def clean_invisibles(element: ET.Element, parent: ET.Element):
         for child in list(element):
             clean_invisibles(child, element)
 
+def clean_flow(element: ET.Element, parent: ET.Element):
+    if element.tag.startswith('flow'):
+        parent.remove(element)
+    else:
+        for child in list(element):
+            clean_flow(child, element)
+
 clean_namespaces(root, None)
 clean_invisibles(root, None)
+if angular_mode:
+    clean_flow(root, None)
 tree.write('output.svg')
